01c3d0c19152797cbdd88dd52a97cb9a85a3803b
Soundaryasarashetti <soundaryasarashetti880@gmail.com>
2025-05-13 13:52:39 +0530

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+$CompanyName = "<b>Go Fresh</b>"; 
+
+
+$CompanyLogo = "https://lh3.googleusercontent.com/3OQwwHdr24p7tomSrSxiXkt5kxExHlXzJaMQM4Eq5T-sBnEDZpxig0W-7FrAQvjKGz2mvFFk4-zetsiR19K-Cxt7Ec96zrsdtReuFcSsT0HFZCjnXKW0"; 
+
+
+
+
+
+
+
+
+
+
+?>
+
